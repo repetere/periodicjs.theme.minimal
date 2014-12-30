a5dc9b70bf30a106b02d5fee30ffd63649e0c389
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-12-30 11:56:38 -0500

    updated minimal theme

--- routes.js
+++ routes.js
@@ -1,13 +1,29 @@
 'use strict';
-var path = require('path');
+// var path = require('path');
 
-module.exports = function(periodic){
-	var themeController = require(path.join(__dirname,'../../../app/controller/theme'))(periodic),
-			postController = require(path.join(__dirname,'../../../app/controller/post'))(periodic),
-			themeRouter = periodic.express.Router();
+module.exports = function (periodic) {
+	// var 
+	// 		itemController = periodic.app.controller.native.item,
+	var themeController = periodic.app.controller.native.theme,
+		themeRouter = periodic.express.Router(),
+		envThemeSettings = periodic.settings.themeSettings.settings[periodic.settings.application.environment];
 
-	// create new route to document posts to post
-	themeRouter.get('post/:id',postController.loadPost,postController.show);
+	if (periodic.settings.themeSettings) {
+		periodic.app.locals.showperiodiccredit = envThemeSettings.showperiodiccredit;
+		periodic.app.locals.showadminlink = envThemeSettings.showadminlink;
+		periodic.app.locals.navlinks = envThemeSettings.navlinks;
+		periodic.app.locals.autohidetitle = envThemeSettings.autohidetitle;
+		periodic.app.locals.usecustomnav = envThemeSettings.usecustomnav;
+		periodic.app.locals.customnav = envThemeSettings.customnav;
+	}
+
+	if (envThemeSettings.navlinks === 'categories') {
+		themeRouter.get('/browse/:entitytype|/browse/:entitytype/:entityitems|/author/:id|/search|/404|/notfound|/collections|/collection/:id|/collection/search|/items|/item/search|/articles|/item/:id|/', themeController.loadNavCategories, themeController.getNavCategories);
+	}
+	// // create new route to document items to post
+	themeRouter.get('/', themeController.setCacheHeader, themeController.homepagedata, themeController.homepageindex);
+
 
 	periodic.app.use(themeRouter);
+	return periodic;
 };
